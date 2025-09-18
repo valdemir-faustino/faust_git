5cff8bf00ed6307e29a50b6956efb2aaa59f98e5
Valdemir-Faustino <valfaustino2@hotmail.com>
2025-09-18 11:53:01 -0300

    Correção da rota edit

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [RotaController::class, 'index'])->name('index');
 Route::get('/create', [RotaController::class, 'create'])->name('create');
 Route::post('/store', [RotaController::class, 'store'])->name('store');
-Route::get('/edit{id}', [RotaController::class, 'edit{id}'])->name('edit{id}');
+Route::get('/edit{id}', [RotaController::class, 'edit'])->name('edit');
 Route::put('/update{id}', [RotaController::class, 'update'])->name('update');
 Route::get('/show{id}', [RotaController::class, 'show'])->name('show');
 Route::delete('/destroy{id}', [RotaController::class, 'destroy'])->name('destroy');
