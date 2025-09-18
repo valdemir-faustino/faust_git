e8186a5cd70bf96d19f2fb4a651a0d978581a2d3
Valdemir-Faustino <valfaustino2@hotmail.com>
2025-09-18 11:50:17 -0300

    Implementação e criação das rota

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/', [RotaController::class, 'index'])->name('index');
+Route::get('/create', [RotaController::class, 'create'])->name('create');
+Route::post('/store', [RotaController::class, 'store'])->name('store');
+Route::get('/edit{id}', [RotaController::class, 'edit{id}'])->name('edit{id}');
+Route::put('/update{id}', [RotaController::class, 'update'])->name('update');
+Route::get('/show{id}', [RotaController::class, 'show'])->name('show');
+Route::delete('/destroy{id}', [RotaController::class, 'destroy'])->name('destroy');
